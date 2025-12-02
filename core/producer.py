370e06ece9b07d2eaa898cbe3742094d6bfdc386
Sean Broderick <talos6723@gmail.com>
2025-12-02 17:58:30 -0500

    kafka init

--- core/producer.py
+++ core/producer.py
@@ -42,22 +42,17 @@ def track_live(st:yf.tickers.Tickers, ft:yf.tickers.Tickers):
     sthread.join()
     fthread.join()
 
-def pull_historic(st:yf.tickers.Tickers, ft:yf.tickers.Tickers):
-    #historic data for stock for the day (every minute)   
-    print(st.tickers['RTX'].history(period='1d', interval='1m'))
-
 # this func is the main running of the producer
 def run():
     log.info(f'run - Starting Producer Run')
     db.start()
+    
     # tickers we will track for STOCKS in this project will be Palantir, Meta, Google, RTX (4)
     stock_tickers = yf.Tickers(stocks)
     # tickers we will track for ETFs/Funds in this project will be VOO, SWPPX, SPYD, SCHX, SCHD, SCHA (6)
     fund_tickers = yf.Tickers(funds)
+    track_live(stock_tickers, fund_tickers)
 
-
-    #track_live(stock_tickers, fund_tickers)
-    pull_historic(stock_tickers, fund_tickers)
     db.end()
     log.info(f'run - Ending Producer Run')
     
